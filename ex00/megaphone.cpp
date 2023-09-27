c4a54b338e9e14f8d1aeac54ec04f10d11aa0482
Tuomas <tuomas.jaasalo@alceon.fi>
2023-09-27 17:48:03 +0300

    Added ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (EXIT_SUCCESS);
+    }
+    for (int idx_arg = 1; idx_arg < argc; idx_arg++)
+    {
+        for (int idx_str = 0; argv[idx_arg][idx_str]; idx_str++)
+        {
+            argv[idx_arg][idx_str] = toupper(argv[idx_arg][idx_str]);
+        }
+        std::cout << argv[idx_arg];
+    }
+    std::cout << '\n';
+    return (EXIT_SUCCESS);
+}
