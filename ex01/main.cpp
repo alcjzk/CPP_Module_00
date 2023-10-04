8109edbb9e2828e2ba370421971d79560282d19b
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-10-04 07:21:38 +0300

    Cleanup main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,32 +6,24 @@ int main(void)
     PhoneBook   book;
     std::string input;
 
-    try
+    while (true)
     {
-        while (true)
+        std::cout << "Enter command (ADD | SEARCH | EXIT): ";
+        std::getline(std::cin, input);
+        if (input.compare("ADD") == 0)
         {
-            std::cout << "Enter command (ADD | SEARCH | EXIT): ";
-            std::getline(std::cin, input);
-            if (input.compare("ADD") == 0)
-            {
-                Contact contact;
-                contact.fill_prompt();
-                book.add_contact(contact);
-            }
-            else if (input.compare("SEARCH") == 0)
-            {
-                book.display();
-            }
-            else if (input.compare("EXIT") == 0)
-            {
-                return (EXIT_SUCCESS);
-            }
+            Contact contact;
+            contact.fill_prompt();
+            book.add_contact(contact);
         }
-    }
-    catch (int error)
-    {
-        std::cout << "Error" << std::endl;
-        return (EXIT_FAILURE);
+        else if (input.compare("SEARCH") == 0)
+        {
+            book.display();
+        }
+        else if (input.compare("EXIT") == 0)
+        {
+            return (EXIT_SUCCESS);
+        }
     }
     return (EXIT_SUCCESS);
 }
