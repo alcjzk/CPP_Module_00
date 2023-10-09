2292bc31858e407e98c090d07cb2e1a71654d615
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-10-09 03:40:46 +0300

    ex02 setup

---
+++ ex02/Account.cpp
@@ -0,0 +1,69 @@
+#include "Account.hpp"
+
+int	Account::getNbAccounts( void )
+{
+	return (-1); // TODO: 
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (-1); // TODO: 
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (-1); // TODO: 
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (-1); // TODO: 
+}
+
+
+void	Account::displayAccountsInfos( void )
+{
+	// TODO: 
+}
+
+Account::Account( int initial_deposit )
+{
+	// TODO: 
+}
+
+Account::~Account( void )
+{
+	// TODO: 
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	// TOOD: 
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	// TODO:
+	return (false);
+}
+
+int		Account::checkAmount( void ) const
+{
+	// TODO: 
+	return (-1);
+}
+
+void	Account::displayStatus( void ) const
+{
+	// TODO:
+}
+
+void	Account::_displayTimestamp( void )
+{
+	// TODO: 
+}
+
+Account::Account( void )
+{
+	// TODO: 
+}
