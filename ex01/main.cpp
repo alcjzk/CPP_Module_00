8d527823d76a78d1d149b2499a088db1691ca4c9
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-10-11 03:54:52 +0300

    Handle stdin EOF

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,24 +6,33 @@ int main(void)
     PhoneBook   book;
     std::string input;
 
-    while (true)
+    std::cin.exceptions(std::ios_base::badbit | std::ios_base::eofbit);
+    try
     {
-        std::cout << "Enter command (ADD | SEARCH | EXIT): ";
-        std::getline(std::cin, input);
-        if (input.compare("ADD") == 0)
+        while (true)
         {
-            Contact contact;
-            contact.fill_prompt();
-            book.add_contact(contact);
+            std::cout << "Enter command (ADD | SEARCH | EXIT): ";
+            std::getline(std::cin, input);
+            if (input.compare("ADD") == 0)
+            {
+                Contact contact;
+                contact.fill_prompt();
+                book.add_contact(contact);
+            }
+            else if (input.compare("SEARCH") == 0)
+            {
+                book.display();
+            }
+            else if (input.compare("EXIT") == 0)
+            {
+                return (EXIT_SUCCESS);
+            }
         }
-        else if (input.compare("SEARCH") == 0)
-        {
-            book.display();
-        }
-        else if (input.compare("EXIT") == 0)
-        {
-            return (EXIT_SUCCESS);
-        }
+    }
+    catch (std::exception const &_e)
+    {
+        std::cout << std::endl;
+        return (EXIT_SUCCESS);
     }
     return (EXIT_SUCCESS);
 }
